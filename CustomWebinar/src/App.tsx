88c9de0fe71f14c6b7de5a80f673172db61d43ab
ringku1 <ringkuxinha@gmail.com>
2025-08-01 10:36:27 +0600

    token given manually

--- CustomWebinar/src/App.tsx
+++ CustomWebinar/src/App.tsx
@@ -11,9 +11,12 @@ function App() {
 
   useEffect(() => {
     async function initalizeMeeting() {
-      const searchParams = new URL(window.location.href).searchParams;
+      //const searchParams = new URL(window.location.href).searchParams;
+      //const authToken = searchParams.get("authToken");
+      //use the Auth token directly rather extracting from the URL
 
-      const authToken = searchParams.get("authToken");
+      const authToken =
+        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdJZCI6IjRkMzhjZWRkLWVkZjItNDk3Ny04ZWFjLWI3MzNjZmI2OThiMCIsIm1lZXRpbmdJZCI6ImJiYmQyOTlmLWEwYjQtNDAwOC04MTQ5LTQxOGJjMDViZDUyMiIsInBhcnRpY2lwYW50SWQiOiJhYWFiY2NjYy04MzQ3LTQ3YWQtOWIyNC0yOWIyMmMwOWM5MzciLCJwcmVzZXRJZCI6IjBjZmZhYWQ5LWYwZjYtNDMzYS05Zjg5LTRjYTE0ZTUxNDRhNiIsImlhdCI6MTc1MzQyMTMzOSwiZXhwIjoxNzYyMDYxMzM5fQ.kM3sTZVDrcDtsUn2RW4p5qI6qTXxoJ3Qj-2m2SBRZmdx8QW8giqqcZ9VrMJgq0U3PcSJ6ylEu6npH-GRKsqzui5q2o-M8WejYAHy1gc0LIkbzFDOH9U_0l8-XG2lPmkwJ4c5b-qxSRR8jzNdBtowP14ZTXZKshLAJ9s3lLC1LbOskv9W4U6OgFVgqLtvHB3ltDz4foGns0PEhKDjcn0N64K-fHgeFIKfaydubhZZKThqDTVk1jcOZAvIyXdaC9TNJnjBY51nYhb2lOF6UxrmOmId7qOjp5CXKVaJR92t-17wYKP2QhtgMsqnj6lidD1bAOiqGE1J_FArkpaN_L3PdQ";
 
       if (!authToken) {
         alert(
